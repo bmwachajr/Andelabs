74efa0701a4735e657446668bbca3d776b7a4067
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-19 10:30:14 +0300

    defined function word count in WordCount.

--- WordCount/wordCount.py
+++ WordCount/wordCount.py
@@ -7,4 +7,19 @@ Disc: A python app that counts the occurrence of a word in a string
 
 """
 
-def word_count():
+def word_count(string):
+  dict = {}
+  list = string.split()
+  list.sort()
+  print (list)
+  for word in list:
+    if word in dict:
+      dict[word] = dict[word] + 1
+    else:
+      dict[word] = 1
+  
+  print (dict)    
+  
+  
+string = "testing testing 1 2 testing"
+word_count(string)
