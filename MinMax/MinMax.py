b414227d6a6f4b7570f4401753f9f9e7ed7f4594
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-19 11:45:09 +0300

    min max python code and testcase. working beta

---
+++ MinMax/MinMax.py
@@ -0,0 +1,15 @@
+"""
+
+Name : Min Max app
+Author: Benjamin Wacha
+Email: bmwachajr
+Disc: A python app that that returns the min and max number from a list.
+
+"""
+
+def find_max_min(list):
+  max_min = []
+  max_min.append(list[0])
+  max_min.append(list[-1])
+  return (max_min)
+  
