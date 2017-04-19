4febb85253b5d1bbb23435993efe06863a7e3cc7
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-19 10:56:00 +0300

    Added line isdigit() in code to maintain digits as is

--- WordCount/wordCount.py
+++ WordCount/wordCount.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 
 Name : Word Count
 Author: Benjamin Wacha
@@ -7,19 +7,17 @@ Disc: A python app that counts the occurrence of a word in a string
 
 """
 
-def word_count(string):
+def words(string):
   dict = {}
   list = string.split()
   list.sort()
-  print (list)
+  
   for word in list:
+    if word.isdigit():
+      word = int(word)
     if word in dict:
       dict[word] = dict[word] + 1
     else:
       dict[word] = 1
   
-  print (dict)    
-  
-  
-string = "testing testing 1 2 testing"
-word_count(string)
+  return (dict)    
